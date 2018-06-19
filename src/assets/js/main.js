1201016689e91bb272a6bf56204ae664f8a953cc
floresdani <florescaceresdaniela@gmail.com>
2018-06-19 13:03:03 -0400

    cambiando nombres identificadores

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,7 +4,7 @@ btnDash.addEventListener('click', () => {
   document.getElementById('generalResults').style.display = 'none';
 });
 
-const generalInfo = document.getElementById('goGeneral');
+const generalInfo = document.getElementById('generalData');
 generalInfo.addEventListener('click', () => {
   document.getElementById('search').style.display = 'none';
   document.getElementById('generalResults').style.display = 'block';
