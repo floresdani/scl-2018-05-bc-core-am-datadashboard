d77685ba4c89b58ec8b5c1436f3d04ff37e31895
floresdani <florescaceresdaniela@gmail.com>
2018-06-20 13:34:00 -0400

    actualizando main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,14 +5,14 @@ window.onload = () => {
     document.getElementById('generalResults').style.display = 'none';
   });
 
-  const generalInfo = document.getElementById('goGeneral');
+  const generalInfo = document.getElementById('generalData');
   generalInfo.addEventListener('click', () => {
     document.getElementById('search').style.display = 'none';
     document.getElementById('generalResults').style.display = 'block';
   });
 };
 
-const btn = document.getElementById('goGeneral');
+const btn = document.getElementById('generalData');
 const container = document.getElementById('nombres');
 
 const usersJSONS = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
@@ -29,38 +29,3 @@ const renderUsers = info => {
     return render;
   });
 };
-*/
-
-
-function myFunction() {
-  studentSearch.addEventListener('keypress', (event) => {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
-      } else {
-          li[i].style.display = "none";
-
-      }
-  }
-
-
-
-function dataFull() {
-  const btnDash = document.getElementById('dataDash');
-  btnDash.addEventListener('click', () => {
-    document.getElementById('search').style.display = 'block';
-    document.getElementById('generalResults').style.display = 'none';
-  });
-
-  const generalInfo = document.getElementById('generalData');
-  generalInfo.addEventListener('click', () => {
-    document.getElementById('search').style.display = 'none';
-    document.getElementById('generalResults').style.display = 'block';
-  });
-}
