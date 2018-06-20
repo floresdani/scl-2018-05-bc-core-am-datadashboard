885347ad5a5793b6beb213883bd1b7d34a8db53d
floresdani <florescaceresdaniela@gmail.com>
2018-06-20 12:33:25 -0400

    ultimos cambios

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,50 @@
 
 window.onload = () => {
   dataFull();
-}
+  myFunction();
+};
+/*
+const btn = document.querySelector('input');
+const container = document.getElementById('dataResult');
+const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+
+fetch(usersJSON)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    renderUsers(data);
+  });
+
+const renderUsers = data => {
+  btn.addEventListener('click', () => { // si fuera objeto: for in (recorre objetos)
+    const render = data.forEach(element => {
+      // element.name === arreglo[i].name
+      return container.innerHTML += `<p>${element.name}</p>`
+    });
+    return render;
+  });
+};
+*/
+
+
+function myFunction() {
+  studentSearch.addEventListener('keypress', (event) => {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+      } else {
+          li[i].style.display = "none";
+
+      }
+  }
+
+
 
 function dataFull() {
   const btnDash = document.getElementById('dataDash');
@@ -15,5 +58,4 @@ function dataFull() {
     document.getElementById('search').style.display = 'none';
     document.getElementById('generalResults').style.display = 'block';
   });
-
 }
