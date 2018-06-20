368bc0e3a035bf74e7257c62ae609f2d7a810091
floresdani <florescaceresdaniela@gmail.com>
2018-06-20 09:12:13 -0400

    quitando elemento html y modificacion de js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,19 @@
-const btnDash = document.getElementById('dataDash');
-btnDash.addEventListener('click', () => {
-  document.getElementById('search').style.display = 'block';
-  document.getElementById('generalResults').style.display = 'none';
-});
 
-const generalInfo = document.getElementById('generalData');
-generalInfo.addEventListener('click', () => {
-  document.getElementById('search').style.display = 'none';
-  document.getElementById('generalResults').style.display = 'block';
-});
+window.onload = () => {
+  dataFull();
+}
+
+function dataFull() {
+  const btnDash = document.getElementById('dataDash');
+  btnDash.addEventListener('click', () => {
+    document.getElementById('search').style.display = 'block';
+    document.getElementById('generalResults').style.display = 'none';
+  });
+
+  const generalInfo = document.getElementById('generalData');
+  generalInfo.addEventListener('click', () => {
+    document.getElementById('search').style.display = 'none';
+    document.getElementById('generalResults').style.display = 'block';
+  });
+
+}
