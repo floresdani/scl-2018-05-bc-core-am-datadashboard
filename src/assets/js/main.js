a6fb4e6781e0b504965dfe1f66d58c5e7c0fdfb9
Cote <majovardon@gmail.com>
2018-06-20 12:30:09 -0400

    Trabajo en progreso

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,48 @@
-const btnDash = document.getElementById('dataDash');
-btnDash.addEventListener('click', () => {
-  document.getElementById('search').style.display = 'block';
-  document.getElementById('generalResults').style.display = 'none';
-});
+window.onload = () => {
+  const btnDash = document.getElementById('dataDash');
+  btnDash.addEventListener('click', () => {
+    document.getElementById('search').style.display = 'block';
+    document.getElementById('generalResults').style.display = 'none';
+  });
 
-const generalInfo = document.getElementById('goGeneral');
-generalInfo.addEventListener('click', () => {
-  document.getElementById('search').style.display = 'none';
-  document.getElementById('generalResults').style.display = 'block';
-});
+  const generalInfo = document.getElementById('goGeneral');
+  generalInfo.addEventListener('click', () => {
+    document.getElementById('search').style.display = 'none';
+    document.getElementById('generalResults').style.display = 'block';
+  });
+};
+
+const btn = document.getElementById('goGeneral');
+const container = document.getElementById('nombres');
+
+const usersJSONS = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+fetch(usersJSONS)
+  .then(response => response.json())
+  .then(data => {
+    renderUsers(data);
+  });
+const renderUsers = info => {
+  btn.addEventListener('click', () => {
+    const render = info.forEach(element => {
+      return contenedor.innerHTML += `<p>${element.name}</p>`;
+    });
+    return render;
+  });
+};
+
+/*studentSearch.addEventListener('keypress', (event) => {
+  let key = event.which || event.keyCode;
+  if (key === 13) {
+    let name = studentSearch.value;
+    studentSearch.value = '';
+    const usersJSONS = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+    fetch(usersJSONS)
+      .then(response => response.json())
+      .then(data => {
+        renderUsers(data);
+      });
+    const renderUsers = info => {
+      contenedor.innerHTML = info.name;
+    };
+  }
+});*/
