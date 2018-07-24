5e389d42af64ed1674ac7e5ebff81b6680bde98c
floresdani <florescaceresdaniela@gmail.com>
2018-07-24 14:38:58 -0400

    tabla de resultados

--- src/assets/js/funcionalidad.js
+++ src/assets/js/funcionalidad.js
@@ -12,3 +12,18 @@ generalInfo.addEventListener('click', () => {
   document.getElementById('search').style.display = 'none';
   document.getElementById('generalResults').style.display = 'block';
 });
+
+/* Aqui va el listados de  nombres */
+function usersList() {
+
+  const btn = document.getElementById('generalData');
+  const container = document.getElementById('nameList');
+
+  const renderUsers = info => {
+    btn.addEventListener('click', () => {
+      const render = info.forEach(student => {
+        return container.innerHTML += `<ul><b>NOMBRE:</b> ${student.name.toUpperCase()}<br><b>ID:</b> ${student.id}</ul>`;
+      });
+      return render;
+    });
+  };
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,58 +3,29 @@ let progress = null;
 let cohorts = null; // null == false => true
 let usersStats = null;
 
-/* window.onload = () => {
-  users();
-  stats();
-};*/
-/* Aqui va el listados de  nombres */
-function usersList() {
+fetch('../data/cohorts/lim-2018-03-pre-core-pw/users.json')
+  .then(response => response.json())
+  .then(usersJSONS => {
+    users = usersJSONS;
+    areWeFinishedYet();
+  })
+  .catch(error => {
+    console.error('No pudimos obtener usuarios');
+    // console.error indica un mensaje de error, indica una alerta grave
+    console.error('ERROR > ' + error.stack); // error.stack muestra donde falló el codigo, imprime donde esta el error
+  });
 
-  const btn = document.getElementById('generalData');
-  const container = document.getElementById('nameList');
+fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json')
+  .then(response => response.json())
+  .then(progressJSON => {
+    progress = progressJSON;
+    areWeFinishedYet();
+  })
+  .catch(error => {
+    console.error('No se puede obetener el progreso');
+    console.error('ERROR ' + error.stack);
+  });
 
-  const usersJSONS = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-  fetch(usersJSONS)
-    .then(response => response.json())
-    .then(info => {
-      renderUsers(info);
-      areWeFinishedYet();
-    })
-    .catch(error => {
-      console.error('No pudimos obtener usuarios');
-      // console.error indica un mensaje de error, indica una alerta grave
-      console.error('ERROR > ' + error.stack); // error.stack muestra donde falló el codigo, imprime donde esta el error
-    });
-
-  const renderUsers = info => {
-    btn.addEventListener('click', () => {
-      const render = info.forEach(student => {
-        return container.innerHTML += `<ul><b>NOMBRE:</b> ${student.name.toUpperCase()}<br><b>ID:</b> ${student.id}</ul>`;
-      });
-      return render;
-    });
-  };
-}
-
-/* Aquí va el listado de el progreso*/
-function progressUsers() {
-  fetch('../data/cohorts/lim-2018-03-pre-core-pw/progress.json')
-    .then((datas) => {
-      if (datas.ok) {
-        return datas.json();
-      } else {
-        throw new Error('No se pudo procesar la información');
-      }
-    }).then((info) => {
-      console.log((info));
-    })
-    .catch((error) => {
-      console.error('Ha ocurrido un error: ' + error);
-    });
-  const progress = info => {
-    console.log(info);
-  };
-};
 
 /* Función para buscar alumna por nombre */
 searchStudent = () => {
